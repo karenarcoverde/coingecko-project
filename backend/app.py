a58c7f91e32526a28b9cdc6b0c74d47c064d35ba
Karen dos Anjos Arcoverde <59583521+karenarcoverde@users.noreply.github.com>
2026-06-14 18:28:40 -0300

    Add files via upload

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 COINGECKO_URL = "https://api.coingecko.com/api/v3/simple/price"
 
